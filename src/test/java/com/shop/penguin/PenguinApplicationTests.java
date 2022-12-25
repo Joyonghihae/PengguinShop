b2dbf3b9fc5da238ea0b113b0e32e78d5f46421b
Joyonghihae <rladms010@gmail.com>
2022-12-25 02:10:14 -0500

    first commit

---
+++ src/test/java/com/shop/penguin/PenguinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.penguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
